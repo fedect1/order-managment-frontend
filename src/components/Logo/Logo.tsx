14bf775160e68f6b48b3490afcfda46229e32209
CarrilloF <fedecarrillo79@gmail.com>
2025-02-19 09:06:55 +0100

    Add logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+export function Logo() {
+    const router = useRouter()
+  return (
+    <div className="min-h-20 h-20 flex items-center border-r cursor-pointer gap-2 p-3" onClick={()=>router.push("/")}>
+
+        <Image src="/logo.png" alt="Logo" width={30} height={30} priority/>
+
+        <h1 className="font-bold text-xl">Order manager</h1>
+    </div>
+  )
+}
