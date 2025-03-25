68f014e1e4ca5bd95390bd9e9d2713b083b8d5d2
CarrilloF <fedecarrillo79@gmail.com>
2025-03-25 07:57:14 +0100

    Change data example inputs

--- src/app/(routes)/charges/components/silos/SiloSummary/SiloSummary.data.ts
+++ src/app/(routes)/charges/components/silos/SiloSummary/SiloSummary.data.ts
@@ -2,7 +2,7 @@ export const dataSiloSummary = [
     {
         SILO_SILO: 1,
         SILO_NAME: "Silo 1",
-        SILO_QUANTITY: 4500,
+        SILO_QUANTITY: 100,
         SILO_CHARGE: "ARG-01",
         SILO_CAPACITY: 5000,
         RAWMAT_NAME: "PR1",
@@ -11,7 +11,7 @@ export const dataSiloSummary = [
     {
         SILO_SILO: 2,
         SILO_NAME: "Silo 2",
-        SILO_QUANTITY: 4500,
+        SILO_QUANTITY: 3000,
         SILO_CHARGE: "ARG-02",
         SILO_CAPACITY: 5000,
         RAWMAT_NAME: "PR2",
@@ -20,7 +20,7 @@ export const dataSiloSummary = [
     {
         SILO_SILO: 3,
         SILO_NAME: "Silo 3",
-        SILO_QUANTITY: 4500,
+        SILO_QUANTITY: 1000,
         SILO_CHARGE: "ARG-03",
         SILO_CAPACITY: 5000,
         RAWMAT_NAME: "PR3",
@@ -29,7 +29,7 @@ export const dataSiloSummary = [
     {
         SILO_SILO: 4,
         SILO_NAME: "Silo 4",
-        SILO_QUANTITY: 4500,
+        SILO_QUANTITY: 4000,
         SILO_CHARGE: "ARG-04",
         SILO_CAPACITY: 5000,
         RAWMAT_NAME: "PR4",
@@ -38,7 +38,7 @@ export const dataSiloSummary = [
     {
         SILO_SILO: 5,
         SILO_NAME: "Silo 5",
-        SILO_QUANTITY: 4500,
+        SILO_QUANTITY: 3500,
         SILO_CHARGE: "5",
         SILO_CAPACITY: 5000,
         RAWMAT_NAME: "PR5",
@@ -47,7 +47,7 @@ export const dataSiloSummary = [
     {
         SILO_SILO: 6,
         SILO_NAME: "Silo 6",
-        SILO_QUANTITY: 4500,
+        SILO_QUANTITY: 2500,
         SILO_CHARGE: "ARG-06",
         SILO_CAPACITY: 5000,
         RAWMAT_NAME: "PR6",
